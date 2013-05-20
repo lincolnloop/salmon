f627cf3606a4c86ee01c0494d99e6e67d3b6cf88
Yann Malet <yann.malet@gmail.com>
2013-05-20 12:19:20 +0200

    Adding --fake option in the run_checks management command

--- salmon/apps/monitor/management/commands/run_checks.py
+++ salmon/apps/monitor/management/commands/run_checks.py
@@ -1,50 +1,67 @@
 import json
 import subprocess
-from django.core.management.base import NoArgsCommand
+from optparse import make_option
+import yaml
+
+from django.core.management.base import BaseCommand
 from django.utils import timezone
 from django.conf import settings
-import yaml
 
 from salmon.apps.monitor import models, utils
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
     help = "Run Salt monitor checks"
+    option_list = BaseCommand.option_list + (
+        make_option('--fake',
+                    action='store_true',
+                    dest='fake',
+                    default=False,
+                    help='Do not run the checks but print the command instead'),)
 
     def load_salmon_checks(self):
         checks_yaml = open(settings.SALMON_CHECKS_PATH).read()
         return yaml.safe_load(checks_yaml)
 
-
-    def handle_noargs(self, **options):
+    def handle(self, *args, **options):
+        import ipdb; ipdb.set_trace()
         config = self.load_salmon_checks()
-        self.stdout.write("Running checks...")
+        if options['fake']:
+            self.stdout.write("Printing checks [fake mode] ...")
+        else:
+            self.stdout.write("Running checks...")
         for target, functions in config.items():
             for func_name, func_opts in functions.items():
                 cmd = utils.build_command(target, func_name)
-                check, _ = models.Check.objects.get_or_create(
-                    target=target, function=func_name,
-                    name=func_opts.get('name', func_name))
-                self.stdout.write("+ {}".format(' '.join(cmd)))
-                timestamp = timezone.now()
-                result = subprocess.check_output(cmd)
-                check.last_run = timestamp
-                check.active = True
-                check.save()
-                try:
-                    parsed = json.loads(result)
-                except ValueError:
-                    self.stdout.write("  Error parsing results")
-                    continue
-                # parse out minion names in the event of a wildcard target
-                for name, raw_value in parsed.iteritems():
-                    value = utils.parse_value(raw_value, func_opts)
-                    self.stdout.write("  {}: {}".format(name, value))
-                    minion, _ = models.Minion.objects.get_or_create(name=name)
-                    failed = utils.check_failed(value, func_opts)
-                    models.Result.objects.create(timestamp=timestamp,
-                                                 result=value,
-                                                 result_type=func_opts['type'],
-                                                 check=check,
-                                                 minion=minion,
-                                                 failed=failed)
+                if options['fake']:
+                    self.stdout.write("cmd: %s" % cmd)
+                else:
+                    self._run_cmd(cmd)
+
+    def _run_cmd(self, cmd):
+        check, _ = models.Check.objects.get_or_create(
+            target=target, function=func_name,
+            name=func_opts.get('name', func_name))
+        self.stdout.write("+ {}".format(' '.join(cmd)))
+        timestamp = timezone.now()
+        result = subprocess.check_output(cmd)
+        check.last_run = timestamp
+        check.active = True
+        check.save()
+        try:
+            parsed = json.loads(result)
+        except ValueError:
+            self.stdout.write("  Error parsing results")
+            return
+        # parse out minion names in the event of a wildcard target
+        for name, raw_value in parsed.iteritems():
+            value = utils.parse_value(raw_value, func_opts)
+            self.stdout.write("  {}: {}".format(name, value))
+            minion, _ = models.Minion.objects.get_or_create(name=name)
+            failed = utils.check_failed(value, func_opts)
+            models.Result.objects.create(timestamp=timestamp,
+                                         result=value,
+                                         result_type=func_opts['type'],
+                                         check=check,
+                                         minion=minion,
+                                         failed=failed)
