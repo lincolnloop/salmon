b11bcb64f0e2000bffa339c81697e641e5a5be3b
Yann Malet <yann.malet@gmail.com>
2013-05-22 19:14:46 +0200

    Adding a management command to send the generate_sample_data

---
+++ salmon/apps/monitor/management/commands/generate_sample_data.py
@@ -0,0 +1,23 @@
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+
+from salmon.apps.monitor.tests import generate_sample_data
+
+
+class Command(BaseCommand):
+    help = "Run Salt monitor checks"
+    option_list = BaseCommand.option_list + (
+        make_option('--point_number',
+                    action='store_true',
+                    dest='point_numbers',
+                    default=1000,
+                    help='Number of results you want to create.'),
+        make_option('--interval',
+                    action='store_true',
+                    dest='interval',
+                    default=5,
+                    help='Interval in minutes between each result'))
+
+    def handle(self, *args, **options):
+        generate_sample_data(options["point_numbers"], options["interval"])
