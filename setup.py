fe4a9741c1f24045c0657af03d575e64c02a892a
Peter Baumgartner <pete@lincolnloop.com>
2013-08-04 01:11:29 -0500

    Adds missing requirement

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         'logan==0.5.5',
         'gunicorn>=0.17.2,<0.18.0',
         'whisper==0.9.10',
+        'jsonfield==0.9.17',
     ],
     entry_points={
         'console_scripts': [
