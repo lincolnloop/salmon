2f7cab9146d3ce4f3558a80d6b9b228a83a4bfdb
Peter Baumgartner <pete@lincolnloop.com>
2013-11-16 21:19:54 -0600

    Adds missing trailing slash

--- salmon/urls.py
+++ salmon/urls.py
@@ -8,6 +8,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
     url(r'^$', metrics_views.dashboard, name="dashboard"),
-    url(r'^(?P<name>[-\w\._]*)$', metrics_views.history, name="history"),
+    url(r'^(?P<name>[-\w\._]*)/$', metrics_views.history, name="history"),
     url(r'^api/v1/metric/$', metrics_views.CreateMetricView.as_view()),
 )
